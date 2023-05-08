1687b3d53ef554fa0674d9681513a242e5db3712
Sauradip07 <sauradip96ghosh@gmail.com>
2023-05-08 09:56:05 +0530

    getURLsFromHTML function add invalid case and test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,37 @@
+const {JSDOM} = require('jsdom');
+
+function getURLsFromHTML(htmlBody,baseURL){
+     const urls = [];
+     const dom = new JSDOM(htmlBody);
+     const linkElements = dom.window.document.querySelectorAll('a');
+     for(const linkElement of linkElements){
+          // console.log(linkElement.href)
+          if(linkElement.href.slice(0,1) === '/'){
+               //relative url
+               //if url construtor is give a invalid url the throw a error
+               try{
+                    const urlObj = new URL(`${baseURL}${linkElement.href}`);
+                    urls.push(urlObj.href);
+               } catch(err){
+                    console.log(`error with relative url: ${err.message}`);
+               }
+               
+
+               // urls.push(`${baseURL}${linkElement.href}`);
+          }
+          else{
+               //absolute url
+               try{
+                    const urlObj = new URL(linkElement.href);
+                    urls.push(urlObj.href);
+               } catch(err){
+                    console.log(`error with absolute url: ${err.message}`);
+               }
+          }
+          
+     }
+     return urls;
+}
 function normalizerURL(urlString){
      const urlObj = new URL(urlString)
      const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +43,6 @@ function normalizerURL(urlString){
      // return urlString;
 }
 module.exports = {
-     normalizerURL
+     normalizerURL,
+     getURLsFromHTML
 }
