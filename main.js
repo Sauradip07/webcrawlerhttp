1e5b9aa0b89d9ce0f10829785368ee705bf23c41
Sauradip07 <sauradip96ghosh@gmail.com>
2023-05-08 13:39:11 +0530

    crawl singel page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("HelloWorld")
+const {crawlPage} = require('./crawl.js')
+function main(){
+     if(process.argv.length < 3){
+          console.log("no website provied");
+          process.exit(1);
+     }
+     if(process.argv.length > 3){
+          console.log("two many command line arg");
+          process.exit(1);
+     }
+     const baseURL = process.argv[2];
+
+     // for(const arg of process.argv){
+     //      console.log(arg);
+     // }
+     console.log(`starting crawl ${baseURL}`);
+     crawlPage(baseURL)
+}
+main();
